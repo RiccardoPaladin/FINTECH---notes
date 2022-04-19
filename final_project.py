a84f13d9afd1cf47ba79fe37c65184757f4be5b4
RiccardoPaladin <rpaladin@stevens.edu>
2022-04-19 14:04:28 -0400

    final project

--- final_project.py
+++ final_project.py
@@ -6,45 +6,43 @@ import matplotlib.pyplot as plt
 from pandas_datareader import data
 from datetime import date
 import yfinance as yf
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error
-from sklearn.pipeline import Pipeline
-from sklearn.linear_model import Ridge
-from sklearn import linear_model
-from sklearn.linear_model import Lasso
 import seaborn as sn
-from sklearn.preprocessing import PolynomialFeatures
-from sklearn.pipeline import Pipeline
-import statsmodels.api as sm
 
+st.set_page_config(
+    page_title="Stock fundamental analysis")
 
 st.title('Stock fundamental analysis')
 st.text('In this web app you can insert stock tickers and obtain several results...')
 
 st.text('Insert a series of tickers with comma')
 
-tickers_input = st.text_input('Enter here the tickers','')
-start_date = st.text_input('Enter here the start date','')
-end_date = st.text_input('Enter here the end date','')
-
+#tickers_input =list(st.text_input('Enter here the tickers',''))
+#start_date = st.text_input('Enter here the start date','')
+#end_date = st.text_input('Enter here the end date','')
+start_date = '01-01-2020'
+end_date = '03-28-2022'
+tickers_input = ['SPY', 'AAPL']
 Data = data.DataReader(tickers_input, 'yahoo', start_date, end_date)
 Stocks_prices = Data['Adj Close']
 all_weekdays = pd.date_range(start=start_date, end=end_date, freq='B')
 Stocks_prices = Stocks_prices.reindex(all_weekdays)
 Stocks_prices = Stocks_prices.fillna(method='ffill')
-prices = Stocks_prices.head()
+Stocks_price = st.dataframe(Stocks_prices)
 
 st.markdown(
     f"""
-    {prices}
+    {Stocks_price}
     """
 )
 
-Stocks = Stocks_prices.pct_change()
-Returns = Stocks.dropna()
+#Stocks_prices = pd.DataFrame(Stocks_prices)
+corrMatrix = Stocks_prices.corr()
+plt.figure(figsize=(15,10))
+plot = st.pyplot(sn.heatmap(corrMatrix, annot=False))
+
 
 st.markdown(
     f"""
-    {Returns}
+    {plot}
     """
 )
