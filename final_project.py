ba9846f8ae43b4a262d6e2c41c6aca68f58a1988
RiccardoPaladin <rpaladin@stevens.edu>
2022-04-19 20:04:19 -0400

    final project

--- final_project.py
+++ final_project.py
@@ -7,6 +7,7 @@ from pandas_datareader import data
 from datetime import date
 import yfinance as yf
 import seaborn as sn
+from sklearn.linear_model import LinearRegression
 
 st.set_page_config(
     page_title="Stock fundamental analysis")
@@ -21,7 +22,7 @@ st.text('Insert a series of tickers with comma')
 #end_date = st.text_input('Enter here the end date','')
 start_date = '01-01-2020'
 end_date = '03-28-2022'
-tickers_input = ['SPY', 'AAPL']
+tickers_input = ['SPY', 'AAPL', 'TSLA']
 Data = data.DataReader(tickers_input, 'yahoo', start_date, end_date)
 Stocks_prices = Data['Adj Close']
 all_weekdays = pd.date_range(start=start_date, end=end_date, freq='B')
@@ -35,14 +36,71 @@ st.markdown(
     """
 )
 
-#Stocks_prices = pd.DataFrame(Stocks_prices)
-corrMatrix = Stocks_prices.corr()
-plt.figure(figsize=(15,10))
-plot = st.pyplot(sn.heatmap(corrMatrix, annot=False))
-
+price_chart = st.line_chart(pd.DataFrame(Stocks_prices['SPY']))
 
 st.markdown(
     f"""
-    {plot}
+    {price_chart}
     """
 )
+
+
+
+Stocks = Stocks_prices.pct_change()
+Stocks = Stocks.dropna()
+
+fundamentals = []
+mean_ret = []
+for (columnName, columnData) in Stocks.iteritems():
+    means = columnData.mean() * 252
+    mean_ret.append(means)
+    stds = columnData.std() * (252 ** 0.5)
+    sharpe = means / stds
+    fundamentals.append(
+        {'Stock': columnName,
+         'Mean': means,
+         'Standard Dev': stds,
+         'Sharpe': sharpe
+
+         }
+    )
+
+reg_data = []
+for i in range(3):
+    model = LinearRegression()
+    X = Stocks.iloc[0:, 0].to_numpy().reshape(-1, 1)
+    Y = Stocks.iloc[0:, i].to_numpy().reshape(-1, 1)
+    reg = model.fit(X, Y)
+    alpha = float(reg.intercept_)
+    beta = float(reg.coef_)
+    reg_data.append(
+        {'Alpha': alpha,
+         'Beta': beta
+
+         })
+fundamentals = pd.DataFrame(fundamentals)
+reg_data = pd.DataFrame(reg_data)
+fundamentals = fundamentals.join(reg_data)
+fundamentals = fundamentals.sort_values(by='Sharpe', ascending=False)
+fundamental = st.dataframe(fundamentals)
+st.markdown(
+    f"""
+    {fundamental}
+    """
+)
+
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+fig, ax = plt.subplots()
+sns.heatmap(Stocks.corr(), ax=ax)
+corr = st.write(fig)
+
+st.markdown(
+    f"""
+    {corr}
+    """
+)
+
+
